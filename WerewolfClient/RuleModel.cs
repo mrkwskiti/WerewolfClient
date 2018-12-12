8d2c2abe3015ac991e7f8c44d9732b2f1988f286
sasirat <sasirat019@gmail.com>
2018-12-12 14:44:56 +0700

    update RuleModel

--- WerewolfClient/RuleModel.cs
+++ WerewolfClient/RuleModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace WerewolfClient
 {
-    class RuleModel
+    class RuleModel : Model
     {
+        public const string RULE_SEER = "Seer";
+        public const string RULE_AURA_SEER = "Aura Seer";
+        public const string RULE_PRIEST = "Priest";
+        public const string RULE_DOCTOR = "Doctor";
+        public const string RULE_WEREWOLF = "Werewolf";
+        public const string RULE_WEREWOLF_SHAMAN = "Werewolf Sharman";
+        public const string RULE_ALPHA_WEREWOLF = "Alpha Werewolf";
+        public const string RULE_WEREWOLF_SEER = "Werewolf Seer";
+        public const string RULE_MEDIUM = "Medium";
+        public const string RULE_BODYGUARD = "Bodyguard";
+        public const string RULE_JAILER = "Jailer";
+        public const string RULE_FOOL = "Fool";
+        public const string RULE_HEAD_HUNTER = "Head Hunter";
+        public const string RULE_SERIAL_KILLER = "Serial Killer";
+        public const string RULE_GUNNER = "Gunner";
     }
 }
