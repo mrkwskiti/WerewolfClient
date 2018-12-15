312a47b1e1f2eb6b8ed0012b7dbed96520561b99
sasirat <sasirat019@gmail.com>
2018-12-15 12:04:38 +0700

    Add view connect to controller

--- WerewolfClient/Rule.cs
+++ WerewolfClient/Rule.cs
@@ -39,7 +39,54 @@ namespace WerewolfClient
 
         private void btnGetRule_click(object sender, PaintEventArgs e)
         {
-
+            RuleCommand rcmd = new RuleCommand();
+            rcmd.Action = RuleCommand.CommandEnum.GetRule;
+            switch (((Button)sender).Name)
+            {
+                case "btnWerewolf":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.WEREWOLF } };
+                    break;
+                case "btnWerewolf_shaman":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.WEREWOLF_SHAMAN } };
+                    break;
+                case "btnAlpha_werewolf":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.ALPHA_WEREWOLF } };
+                    break;
+                case "btnSeer":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.SEER} };
+                    break;
+                case "btnAura_seer":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.AURA_SEER } };
+                    break;
+                case "btnBodyguard":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.BODYGUARD } };
+                    break;
+                case "btnDoctor":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.DOCTOR } };
+                    break;
+                case "btnPriest":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.PRIEST} };
+                    break;
+                case "btnMedium":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.MEDIUM} };
+                    break;
+                case "btnHead_hunter":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.HEAD_HUNTER } };
+                    break;
+                case "btnSerial_killer":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.SERIAL_KILLER } };
+                    break;
+                case "btnFool":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.FOOL } };
+                    break;
+                case "btnJailer":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.JAILER } };
+                    break;
+                case "btnGunner":
+                    rcmd.Payloads = new Dictionary<string, string> { { "Character", RuleModel.GUNNER } };
+                    break;
+            }
+            controller.ActionPerformed(rcmd);
         }
 
     }
