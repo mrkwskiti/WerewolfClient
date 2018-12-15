0b5a03ae929be1f8afd70817d42baac65ca60a69
sasirat <sasirat019@gmail.com>
2018-12-15 10:35:21 +0700

    add Action in CommandEnum add ActionPerformed

--- WerewolfClient/RuleController.cs
+++ WerewolfClient/RuleController.cs
@@ -12,6 +12,8 @@ namespace WerewolfClient
         {
             GetRule = 1
         };
+
+        public new CommandEnum Action { get; set; }
     }
     class RuleController : Controller
     {
@@ -20,5 +22,23 @@ namespace WerewolfClient
         {
             
         }
+
+        public override void ActionPerformed(Command cmd)
+        {
+            foreach(Model m in mList)
+            {
+                if(m is RuleModel && cmd is RuleCommand)
+                {
+                    RuleCommand rcmd = (RuleCommand)cmd;
+                    RuleModel rm = (RuleModel)m;
+                    switch (rcmd.Action)
+                    {
+                        case RuleCommand.CommandEnum.GetRule:
+                        break;
+                    }
+                }
+            }
+        }
+
     }
 }
